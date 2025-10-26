9cccf5079d2a82c80ab74e5d0ce19fd6b80b33bc
AntonioLorussoR <a.lorusso45@studenti.poliba.it>
2025-10-26 14:33:51 +0100

    Update materialRoutes.js

--- routes/materialRoutes.js
+++ routes/materialRoutes.js
@@ -6,9 +6,9 @@ import { deleteMaterial } from "../controllers/materialController.js";
 
 const router = express.Router();
 
-router.post("/:teamId/materials", verifyToken, upload.single("file"), uploadMaterial);
+
 router.get("/teams/:teamId/materials", verifyToken, getMaterials);
 router.post("/teams/:teamId/materials", verifyToken, upload.single("file"), uploadMaterial);
-router.delete("/materials/:materialId", verifyToken, deleteMaterial);
+router.delete("/teams/:teamId/materials/:materialId", verifyToken, deleteMaterial);
 
 export default router;
