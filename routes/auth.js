923c4c68c38475715a40e25f64366826d66a0181
AntonioLorussoR <a.lorusso45@studenti.poliba.it>
2025-10-31 14:23:54 +0100

    Update auth.js

--- routes/auth.js
+++ routes/auth.js
@@ -86,7 +86,7 @@ router.get(
       res.cookie("refreshToken", refreshToken, {
         httpOnly: true,
         secure: true,
-        sameSite: "Strict",
+        sameSite: "None",
         maxAge: 7 * 24 * 60 * 60 * 1000,
       });
 
