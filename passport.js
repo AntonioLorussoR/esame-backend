e8fe9e24171937dbbb102a42a46608ad611edb7a
AntonioLorussoR <a.lorusso45@studenti.poliba.it>
2025-11-02 10:32:33 +0100

    Update passport.js

--- passport.js
+++ passport.js
@@ -40,7 +40,5 @@ passport.use(
     }
   )
 );
-passport.serializeUser((user, done) => done(null, user.id));
-passport.deserializeUser((id, done) => {
-  User.findById(id).then((user) => done(null, user));
+
 });
