3ad7fd1c7ebed209253d50bcaef00d94b2857b06
AntonioLorussoR <a.lorusso45@studenti.poliba.it>
2025-11-02 16:29:14 +0100

    Update auth.js

--- routes/auth.js
+++ routes/auth.js
@@ -3,40 +3,20 @@ import { registerUser, loginUser} from "../controllers/authController.js";
 import { getCurrentUser } from "../controllers/userController.js";
 import { verifyToken } from "../middleware/authMiddleware.js";
 import multer from "multer";
-import path from "path";
 import passport from "passport";
 import jwt from "jsonwebtoken";
-
-const router = express.Router();
-
 import dotenv from "dotenv";
+import storage from "../utils/storage.js";
+
 dotenv.config();
-
-// Configurazione multer per upload immagini profilo
-const storage = multer.diskStorage({
-  destination: (req, file, cb) => {
-    cb(null, "uploads/profilePics");
-  },
-  filename: (req, file, cb) => {
-    const ext = path.extname(file.originalname);
-    cb(null, `${Date.now()}-${file.fieldname}${ext}`);
-  },
-});
-
+const router = express.Router();
 const upload = multer({ storage });
 
 
 router.post("/register", registerUser);
-
-
 router.post("/login", loginUser);
-
-
 router.get("/me", verifyToken, getCurrentUser);
-
-
 router.get("/google", passport.authenticate("google", { scope: ["profile", "email"] }));
-
 router.get("/refresh", (req, res) => {
   const refreshToken = req.cookies.refreshToken;
   if (!refreshToken) {
