5d70f576ba8ea3bb09241a99062a7fa653330868
AntonioLorussoR <a.lorusso45@studenti.poliba.it>
2025-11-02 18:02:50 +0100

    Update user.js

--- routes/user.js
+++ routes/user.js
@@ -1,14 +1,11 @@
 import express from "express";
 import { getCurrentUser, updateUserProfile, uploadProfileImage, deleteAccount, removeProfilePicture} from "../controllers/userController.js";
 import { verifyToken } from "../middleware/authMiddleware.js";
-import { registerUser, loginUser } from "../controllers/authController.js";
 import uploadProfilePic from "../middleware/updateProfilePic.js";
 
 
 const router = express.Router();
 
-router.post("/register", registerUser);
-router.post("/login", loginUser);
 router.put("/me", verifyToken, updateUserProfile);
 router.post("/me/upload", verifyToken, uploadProfilePic.single("profilePicture"), uploadProfileImage);
 router.get("/me", verifyToken, getCurrentUser);
